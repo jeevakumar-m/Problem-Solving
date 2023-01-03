9a0dd7713bf738eaf21daffe48302cadf5ef08da
Jeevakumar Moorthy <96841870+jeevakumar-m@users.noreply.github.com>
2023-01-03 11:06:05 +0530

    Time: 20 ms (30.11%), Space: 48.7 MB (9.21%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
-        int prev=10;
+        
         int c=0;
         for(int i=0;i<strs[0].length();i++)
         {
-            System.out.println("next column");
-            prev=0;
+
+            int prev=0;
             for(int j=0;j<strs.length;j++)
             {
-                System.out.println(String.valueOf(strs[j].charAt(i)) + "," + (char)prev);
+              
                 if(strs[j].charAt(i)<prev)
                 {
                     
