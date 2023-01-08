3a5213ebdb9f70b474709a453e8f6311ba733694
Jeevakumar Moorthy <96841870+jeevakumar-m@users.noreply.github.com>
2023-01-09 00:26:22 +0530

    Time: 37 ms (90.70%), Space: 56.7 MB (80.86%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+         Arrays.sort(costs);
+        int n = costs.length;
+        int answer = 0;
+        // Pick ice creams till we can.
+        while (answer < n && costs[answer] <= coins) {
+            // We can buy this icecream, reduce the cost from the coins. 
+            coins -= costs[answer];
+            answer += 1;
+        }
+        return answer;
+    }
+}
